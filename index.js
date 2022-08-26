362990ccdc5879d632105903b4218c77e8f6272a
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-08-26 12:44:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching (drivers, data) {
+ return drivers.filter(
+    (driverName) => driverName.toLowerCase() === data.toLowerCase()
+ );
+}
+function fuzzyMatch(drivers, data) {
+    return drivers.filter((driverName) => driverName.startsWith(data));
+}
+function matchName(drivers, data) {
+    return drivers.filter(driverName => driverName.name === data)
+}
